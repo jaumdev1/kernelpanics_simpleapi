5db751d59ae5135de0465b772037433aa5ad49c2
jaumdev1 <joao280799@gmail.com>
2024-04-23 19:33:23 -0300

    fix: err cors

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cors = require('cors');
 const app = express();
 const corsOptions = {
-    origin: 'http://localhost:5000', 
+    origin: 'https://kernelpanic.club', 
     optionsSuccessStatus: 200 
 };
   app.use(cors(corsOptions));
